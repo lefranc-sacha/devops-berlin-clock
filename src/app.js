23aa133c6e0a96b03ca815f59ba2c43d6cc88fc8
Ismo <ismet.ismet@student.vinci.be>
2024-11-12 23:57:31 +0100

    add: simpleMinutesLine method with test for 1 minutes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 export class Main{
 
     simpleMinutesLine(minutes){
+
+        if (minutes === 0) return "OOOO";
+        
+        if (minutes === 1) return "YOOO";
         
         return "OOOO";
     }
